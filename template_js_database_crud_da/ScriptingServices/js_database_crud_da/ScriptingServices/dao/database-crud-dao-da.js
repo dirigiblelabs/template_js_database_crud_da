24ae1648664678153091ad5c0ed0110506106e64
shturec <sthrakal@gmail.com>
2017-02-24 11:37:35 +0200

    first draft of support for associations (1-n)

--- template_js_database_crud_da/ScriptingServices/js_database_crud_da/ScriptingServices/dao/database-crud-dao-da.js
+++ template_js_database_crud_da/ScriptingServices/js_database_crud_da/ScriptingServices/dao/database-crud-dao-da.js
@@ -10,7 +10,15 @@ var orm = {
 			dbName: '$tableColumn.name.toUpperCase()',
 #if ($tableColumn.key)
 			id: true,
-			required: true,
+#end
+#if ($tableColumn.size)
+			size: $size,
+#end
+#if ($tableColumn.unique)
+			unique: $unique,
+#end
+#if ($tableColumn.required)
+			required: $required,
 #end
 #if ($tableColumn.type == $INTEGER)
 			type: 'Int'
@@ -40,6 +48,39 @@ var orm = {
 		},
 #end
     ]
+#if ($associations)
+    ,
+    associationSets: {
+#foreach ($association in $associations)
+		"$association.name": {
+#if ($association.multiplicity == $MANY_TO_MANY)
+			"associationType": 'many-to-many',
+#if ($association.key)
+			"key": "$association.key",
+#end			
+#if ($association.joinKey)
+			"joinKey": "$association.joinKey",
+#end			
+#if ($association.daoJoinPath)
+			"daoJoin": require('${packageName}/dao/${association.daoJoin}Dao').get,
+#end
+#if ($association.daoNPath)
+			"daoN": require('${packageName}/dao/${association.daoN}Dao').get,
+#end
+#elseif ($association.multiplicity == $ONE_TO_MANY)
+			"associationType": 'one-to-many',
+			"dao": require('${packageName}/dao/${association.dao}Dao').get,
+#if ($association.key)
+			"key": "$association.key",
+#end
+#if ($association.joinKey)
+			"joinKey": "$association.joinKey",
+#end
+#end
+		},
+#end
+    }
+#end   
 };
 
 exports.get = function() {
